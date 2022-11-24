c95ec51752d200ef4df92689830d8193cd752713
RENNES\g.deleporte <gabysouri@gmail.com>
2022-11-24 15:27:31 +0100

    c'est bon, Seb est Victor, vous êtes content??????

--- projet trans/Assets/Scripts/texteDialogue.cs
+++ projet trans/Assets/Scripts/texteDialogue.cs
@@ -1,20 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class texteDialogue : MonoBehaviour
 {
     public List<string> mesDialogues;
+    public TextMeshProUGUI monTexte;
     public int numeroDialogue = 0;
+    private bool stayTouch = true;
+
+    private int lettreAffiche = 0;
+    private string textAffiche;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (textAffiche != mesDialogues[numeroDialogue])
+        {
+            textAffiche += mesDialogues[numeroDialogue][lettreAffiche];
+        }
+        lettreAffiche++;
+
+        monTexte.text = textAffiche;
+
+        if ((Input.touchCount > 0) && (stayTouch == false) && (textAffiche == mesDialogues[numeroDialogue]))
+        {
+            numeroDialogue = Mathf.Clamp(numeroDialogue + 1, 0, mesDialogues.Count - 1);
+
+            stayTouch = true;
+
+            textAffiche = "";
+            lettreAffiche = 0;
+        }
+        if (Input.touchCount == 0)
+        {
+            stayTouch = false;
+        }
     }
 }
