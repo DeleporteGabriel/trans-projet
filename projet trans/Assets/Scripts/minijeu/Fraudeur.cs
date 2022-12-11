48d0f9e311fbf112e3751815dd280c9514224443
DeleporteGabriel <gabysouri@gmail.com>
2022-12-11 19:00:12 +0100

    les fraudeurs

---
+++ projet trans/Assets/Scripts/minijeu/Fraudeur.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fraudeur : MonoBehaviour
+{
+    public bool isFraudeur = false;
+    public float vitesse;
+
+    public SpriteRenderer sr;
+
+    public float timerMax;
+    public float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Random.Range(0, 2) == 0)
+        {
+            isFraudeur = true;
+        }
+
+        vitesse = Random.Range(0.01f, 0.015f);
+        timerMax = Random.Range(1.7f, 2.2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= timerMax)
+        {
+            if (isFraudeur)
+            {
+                sr.color = Color.red;
+            }
+            else
+            {
+                sr.color = Color.green;
+            }
+        }
+        transform.position += Vector3.down * vitesse;
+
+        if (Physics.Raycast(transform.position, new Vector3(0, 0, 1), out var otherB))
+        {
+            if (otherB.collider.GetComponent<TapFraude>() != null)
+            {
+                if (sr.enabled == true)
+                {
+                    if (isFraudeur == true)
+                    {
+                        otherB.collider.GetComponent<TapFraude>().fraudeCount++;
+                    }
+                    sr.enabled = false;
+                }
+            }
+        }
+    }
+}
---
+++ projet trans/Assets/Scripts/minijeu/TapFraude.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapFraude : MonoBehaviour
+{
+    public bool activeTouch = false;
+
+    public int fraudeCount;
+
+    public float timerMax;
+    private float timer;
+
+    public GameObject mesFraudeurs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= timerMax)
+        {
+            Instantiate(mesFraudeurs, new Vector3(Random.Range(-1.5f, 1.7f), 7, 0), Quaternion.identity);
+            timer = 0;
+        }
+
+        if (Input.touchCount > 0)
+        {
+            var tempPosition = Input.touches[0].position;
+
+            var tempRay = Camera.main.ScreenPointToRay(new Vector3(tempPosition.x, tempPosition.y, Camera.main.nearClipPlane)); //crée un rayon depuis le touch 0
+            if (Physics.Raycast(tempRay, out var other) && activeTouch == false)
+            {
+                if (other.collider.GetComponent<Fraudeur>() != null)
+                {
+                    var targetObject = other.collider.GetComponent<Fraudeur>();
+
+                    if (targetObject.isFraudeur == true)
+                    {
+                        targetObject.sr.enabled = false;
+                    }
+                }
+            }
+
+            activeTouch = true;
+        }
+        else
+        {
+            activeTouch = false;
+        }
+
+        if (fraudeCount >= 3)
+        {
+            Debug.Log("t'as perdu");
+        }
+    }
+}
