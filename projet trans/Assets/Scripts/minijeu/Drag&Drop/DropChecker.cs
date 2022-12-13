d67570f4d14535c3c20a0259d2a44bcbe4e49533
RENNES\g.deleporte <gabysouri@gmail.com>
2022-12-13 13:05:03 +0100

    logo de victor

--- projet trans/Assets/Scripts/minijeu/Drag&Drop/DropChecker.cs
+++ projet trans/Assets/Scripts/minijeu/Drag&Drop/DropChecker.cs
@@ -10,6 +10,10 @@ public class DropChecker : MonoBehaviour
     public int totalDrop;
     public int currentDrop;
     public int isCorrect;
+
+    private bool fini = false;
+
+    public GameObject victor;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,10 +26,15 @@ public class DropChecker : MonoBehaviour
         if (totalDrop == currentDrop && isCorrect == 0)
         {
             //maJaugeValue.jaugeHype += 70;
-            maJaugeValue.AugmenteJaugeValue(1f / 6f);
+            /*maJaugeValue.AugmenteJaugeValue(1f / 6f);
             maJaugeValue.DragDrop = 1;
             maJaugeValue.minijeuTermines++;
-            SceneManager.LoadScene("SceneLightGirl");
+            SceneManager.LoadScene("SceneLightGirl");*/
+            if (fini == false)
+            {
+                Instantiate(victor, new Vector3(0, 1, 0), Quaternion.identity);
+                fini = true;
+            }
         }
     }
 }
