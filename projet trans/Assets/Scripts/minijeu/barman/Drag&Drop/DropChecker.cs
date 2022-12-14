a9493245c71fc8c1e98b3187ab775af63d42d56b
RENNES\v.fabre <victor.fabre@lahiettee.com>
2022-12-14 13:56:39 +0100

    shaker avancement CACA

--- projet trans/Assets/Scripts/minijeu/barman/Drag&Drop/DropChecker.cs
+++ projet trans/Assets/Scripts/minijeu/barman/Drag&Drop/DropChecker.cs
@@ -11,26 +11,53 @@ public class DropChecker : MonoBehaviour
     public int currentDrop;
     public int isCorrect;
 
+    public int diffucltLevel = 1;//niveau de difficulter 1-3 ********INFLUENCE LES TROIS VALEUR SUIVANTE********
+        public int numberGood = 1;//nombre de bon ingredient
+        public int totalNumber = 1;//nombre total d'ingredients
+        public int errortCount = 1;//nombre d'erreur avant echec
+
     private bool fini = false;
 
     public GameObject victor;
 
+    public SpriteRenderer SkinVerre;
+
     public List<GameObject> listeIngredients;
     public List<GameObject> bonIngredient;
     public List<GameObject> mauvaisIngredient;
-
-    
+    public List<Sprite> listSkinVerre;
 
     private bool isTouch = false;
     // Start is called before the first frame update
     void Start()
     {
-        maJaugeValue = FindObjectOfType<IndestructibleObject>();
-        for (var i = 0; i < listeIngredients.Count; i++)
+
+        if (diffucltLevel==1)
         {
-            if (Random.Range(0, 2) == 0)
+            numberGood = 3;
+            totalNumber = 6;
+            errortCount = 3;
+        }
+        if (diffucltLevel == 2)
+        {
+            numberGood = 4;
+            totalNumber = 8;
+            errortCount = 2;
+        }
+        if (diffucltLevel == 3)
+        {
+            numberGood = 6;
+            totalNumber = 12;
+            errortCount = 1;
+        }
+
+        maJaugeValue = FindObjectOfType<IndestructibleObject>();
+        for (var i = 0; i < totalNumber; i++)
+        {
+            if (Random.Range(0, 2) == 0 && numberGood >= 0)
             {
-                bonIngredient.Add(listeIngredients[i]);
+                bonIngredient.Add(listeIngredients[i]) ;
+                numberGood-- ;
             }
             else
             {
@@ -44,15 +71,18 @@ public class DropChecker : MonoBehaviour
 
             if (tempRand == 0)
             {
-                var tempIngredient = Instantiate(mauvaisIngredient[Random.Range(1, mauvaisIngredient.Count)], new Vector3(0 + 0.5f*i, 0, 0), Quaternion.identity);
+                var tempIngredient = Instantiate(mauvaisIngredient[Random.Range(1, mauvaisIngredient.Count)], new Vector3(-1 + 0.5f*i, 4, 0), Quaternion.identity);
                 tempIngredient.GetComponent<DraggedObject>().isGood = false;
             }
             else
             {
-                var tempIngredient = Instantiate(bonIngredient[Random.Range(1, bonIngredient.Count)], new Vector3(0 + 0.5f * i, 0, 0), Quaternion.identity);
+                var tempIngredient = Instantiate(bonIngredient[Random.Range(1, bonIngredient.Count)], new Vector3(-1 + 0.5f * i, 2 , 0), Quaternion.identity);
                 tempIngredient.GetComponent<DraggedObject>().isGood = true;
+                totalDrop++;
             }
         }
+
+        SkinVerre.sprite = listSkinVerre[Random.Range(0, listSkinVerre.Count)];
 
     }
 
